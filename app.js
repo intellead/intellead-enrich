aa791e6b4f83bdee0f0b96bb7efa23e829c92ab5
vinigomes <vinigomes@outlook.com>
2017-07-01 18:53:29 -0700

    first version of lead enrichment, testing with receitaws-data service

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/lead-enrichment', function (req, res) {
     var lead_id = req.body.lead_id;
     //var service = new LeadEnrichmentService(lead_id);
     var service = new LeadEnrichmentService(lead_id, function () {
-        this.enrich(function(result) {
+        service.enrich(function(result) {
             console.log("before send status: " + result);
             res.status(200).send(result);
         });
