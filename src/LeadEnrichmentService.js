fa9e343ac309e20d0b1a3dbdc312c5de5553af50
vinigomes <vinigomes@outlook.com>
2017-07-05 00:29:03 -0700

    test

--- src/LeadEnrichmentService.js
+++ src/LeadEnrichmentService.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var request = require('request');
-var lead_id;
 class LeadEnrichmentService {
 
     constructor(lead_id, email, name, company, cnpj) {
-        lead_id = lead_id;
-        this._id = lead_id;
+        this._lead_id = lead_id;
         this._email = email;
         this._name = name;
         this._company = company;
@@ -24,15 +22,16 @@ class LeadEnrichmentService {
     }
 
     enrichByQcnpjCrawler(callback) {
+        console.log(this._company);
+        console.log(this._lead_id);
         if (this._company) {
             var queryQcnpjCrawler = 'https://qcnpj-crawler.herokuapp.com/?companyName='+this._company;
             request(queryQcnpjCrawler, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var info = JSON.parse(body);
                     console.log(info);
-                    console.log("OUTRO:" + lead_id);
-                    console.log("this._id: " + this._id);
-                    var data = '?lead_id='+this._id+'&rich_information='+info;
+                    console.log("this._id: " + this._lead_id);
+                    var data = '?lead_id='+this._lead_id+'&rich_information='+info;
                     console.log(data);
                     request('https://rdstation-webhook.herokuapp.com/update-enriched-lead-information'+data, function (error, response, body) {
                         console.log("STATUS:"+response.statusCode);
