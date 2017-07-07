6f7f0b89d8c7eb29672f232038dea6044db8e9a1
vinigomes <vinigomes@outlook.com>
2017-07-07 13:57:07 -0700

    logs

--- src/LeadEnrichmentService.js
+++ src/LeadEnrichmentService.js
@@ -18,24 +18,18 @@ class LeadEnrichmentService {
     }
 
     enrichByQcnpjCrawler(id, company_name) {
-        console.log("[enrichByQcnpjCrawler]Entrou no enrichByQcnpjCrawler");
-        console.log("[enrichByQcnpjCrawler]ID: " + id);
         if (company_name) {
             var queryQcnpjCrawler = 'https://qcnpj-crawler.herokuapp.com/?companyName='+company_name;
             request(queryQcnpjCrawler, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var info = JSON.parse(body);
-                    console.log("[enrichByQcnpjCrawler]Retornou uma resposta sem erro: "+ info);
                     request.post(
                         'https://rdstation-webhook.herokuapp.com/update-enriched-lead-information',
                         { json: { lead_id: id, rich_information: info } },
                         function (error, response, body) {
-                            console.log("[enrichByQcnpjCrawler]Enviou para persistir os dados na base");
                             if (error) {
                                 console.log(error);
                             } else if((this._cnpj == null || this._cnpj == undefined) && info.cnpj) {
-                                console.log("[enrichByQcnpjCrawler]Não tem CNPJ na base, mas já recuperou o CNPJ por esse crawler: " + info.cnpj);
-                                console.log("[enrichByQcnpjCrawler]Agora tem info.cnpj: " + info.cnpj);
                                 new LeadEnrichmentService().enrichByReceitaWS(id, info.cnpj);
                              }
                         }
@@ -46,20 +40,15 @@ class LeadEnrichmentService {
     }
 
     enrichByReceitaWS(id, cnpj) {
-        console.log("Entrou no enrichByReceitaWS");
-        console.log("[enrichByReceitaWS]ID: " + id);
-        console.log("[enrichByReceitaWS]_cnpj: " + cnpj);
         if (cnpj) {
             var queryReceitaws = 'https://receitaws-data.herokuapp.com/?cnpj='+cnpj;
             request(queryReceitaws, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var info = JSON.parse(body);
-                    console.log("[enrichByReceitaWS]Retornou resposta sem erro: " + info);
                     request.post(
                         'https://rdstation-webhook.herokuapp.com/update-enriched-lead-information',
                         { json: { lead_id: id, rich_information: info } },
                         function (error, response, body) {
-                            console.log("[enrichByReceitaWS]Enviou para persistir os dados na base");
                             if (error) {
                                 console.log(error);
                             }
