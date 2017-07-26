1f546f3fbfc1a26f4ae8b5e2c662083514d68a7e
Vinícius Gomes <vinigomes@Viniciuss-MacBook-Pro.local>
2017-07-26 14:01:07 -0700

    test

--- app.js
+++ app.js
@@ -37,7 +37,11 @@ var enrich_each_5_minutes = schedule.scheduleJob('*/1 * * * *', function(){
             console.log('intellead-enrich auto Started...');
             if (error || response.statusCode != 200) {
                 console.log('intellead-enrich auto Finished [ERROR]');
-                console.log(error);
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
+                }
             }
             if (!error && response.statusCode == 200) {
                 console.log(body);
