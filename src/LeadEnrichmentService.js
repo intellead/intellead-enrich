b8f02ad0b87f185050ef8f3ce7ed2444d42ab112
Vinícius Gomes <vinigomes@Viniciuss-MacBook-Pro.local>
2017-07-27 18:25:11 -0700

    test

--- src/LeadEnrichmentService.js
+++ src/LeadEnrichmentService.js
@@ -12,7 +12,6 @@ class LeadEnrichmentService {
     enrichByQcnpjCrawler(item) {
         var id = item._id;
         var company_name = item.lead.company;
-        console.log("[enrichByQcnpjCrawler] LEAD_ID: " + id + " | COMPANY: " + company_name);
         if (company_name) {
             var queryQcnpjCrawler = 'https://qcnpj-crawler.herokuapp.com/?companyName='+company_name;
             request(queryQcnpjCrawler, function (error, response, body) {
@@ -26,7 +25,6 @@ class LeadEnrichmentService {
                                 console.log(error);
                             } else if((this._cnpj == null || this._cnpj == undefined) && info.cnpj) {
                                 new LeadEnrichmentService().updateEnrichAttempts('enrichByQcnpjCrawler', id, true);
-                                console.log('[enrichByQcnpjCrawler] Lead '+id+' enriched!');
                                 item.lead.cnpj = info.cnpj;
                                 new LeadEnrichmentService().enrichByReceitaWS(item);
                             }
@@ -58,7 +56,6 @@ class LeadEnrichmentService {
                                 console.log(error);
                             } else {
                                 new LeadEnrichmentService().updateEnrichAttempts('enrichByReceitaWS', id, true);
-                                console.log('[enrichByReceitaWS] Lead '+id+' enriched!');
                             }
                         }
                     );
@@ -76,7 +73,6 @@ class LeadEnrichmentService {
     /*---------------- UTILS ----------------*/
 
     enrichLeadWithAllServices(lead_id, company, cnpj, callback){
-        console.log("[enrichLeadWithAllServices] LEAD_ID: " + lead_id + " | COMPANY: " + company + " | CNPJ: " + cnpj);
         var item = {
             '_id': lead_id,
             'lead' : {
