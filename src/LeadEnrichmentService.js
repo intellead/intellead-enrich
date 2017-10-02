9197368815b3dd9ff803a6fa1c87e2b2e3167ac5
gniehues <gniehues90@gmail.com>
2017-10-01 22:42:13 -0300

    docker links

--- src/LeadEnrichmentService.js
+++ src/LeadEnrichmentService.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var request = require('request');
+var qcnpjCrawlerUrl = process.env.QCNPJ_CRAWLER_URL || 'http://qcnpj-crawler';
+var receitawsDataUrl = process.env.RECEITAWS_DATA_URL || 'http://receitaws-data';
+var dataUpdateEnrichedLeadInfoUrl = process.env.DATA_UPDATE_ENRICHED_LEAD_INFO_URL || 'http://intellead-data/update-enriched-lead-information';
+var dataUpdateEnrichAttempsUrl = process.env.DATA_UPDATE_ENRICH_ATTEMPTS_URL || 'http://intellead-data/update-enrich-attempts';
+var classificationUrl = process.env.CLASSIFICATION_URL || 'http://intellead-classification';
 
 class LeadEnrichmentService {
 
@@ -15,12 +20,12 @@ class LeadEnrichmentService {
         var id = item._id;
         var company_name = item.lead.company;
         if (company_name) {
-            var queryQcnpjCrawler = process.env.QCNPJ_CRAWLER_URL + '/?companyName='+company_name;
+            var queryQcnpjCrawler = qcnpjCrawlerUrl + '/?companyName='+company_name;
             request(queryQcnpjCrawler, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var info = JSON.parse(body);
                     request.post(
-                        process.env.DATA_UPDATE_ENRICHED_LEAD_INFO_URL,
+                        dataUpdateEnrichedLeadInfoUrl,
                         { json: { lead_id: id, rich_information: info } },
                         function (error, response, body) {
                             if (error) {
@@ -51,12 +56,12 @@ class LeadEnrichmentService {
         var that = this;
         var id = item._id;
         if (item.lead && item.lead.cnpj) {
-            var queryReceitaws = process.env.RECEITAWS_DATA_URL + '/?cnpj='+item.lead.cnpj;
+            var queryReceitaws = receitawsDataUrl + '/?cnpj='+item.lead.cnpj;
             request(queryReceitaws, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var info = JSON.parse(body);
                     request.post(
-                        process.env.DATA_UPDATE_ENRICHED_LEAD_INFO_URL,
+                        dataUpdateEnrichedLeadInfoUrl,
                         { json: { lead_id: id, rich_information: info } },
                         function (error, response, body) {
                             if (error) {
@@ -87,7 +92,7 @@ class LeadEnrichmentService {
     classifyIfAllServicesAreReady(service_name, lead_id) {
         this.enrichmentServicesReady.push(service_name);
         if (this.enrichmentServicesReady.indexOf('enrichByQcnpjCrawler') != -1 && this.enrichmentServicesReady.indexOf('enrichByReceitaWS') != -1) {
-            request.get(process.env.CLASSIFICATION_URL + '/' + lead_id);
+            request.get(classificationUrl + '/' + lead_id);
         }
     }
 
@@ -109,7 +114,7 @@ class LeadEnrichmentService {
             [serviceName] : attempts
         };
         request.post(
-            process.env.DATA_UPDATE_ENRICH_ATTEMPTS_URL,
+            dataUpdateEnrichAttempsUrl,
             { json: { lead_id: lead_id, attempts: qtEnrichmentAttempts } },
             function (error, response, body) {
                 if (error) {
