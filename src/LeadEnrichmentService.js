c07e89311715130911834c226caa11bab2ed251b
Vinícius Roggia Gomes <vinigomes@outlook.com>
2017-11-03 11:22:42 -0200

    Update LeadEnrichmentService.js

--- src/LeadEnrichmentService.js
+++ src/LeadEnrichmentService.js
@@ -1,5 +1,23 @@
 'use strict';
 
+/*
+ *
+ * Copyright 2017 Softplan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 var request = require('request');
 var qcnpjCrawlerUrl = process.env.QCNPJ_CRAWLER_URL || 'http://qcnpj-crawler:3000';
 var receitawsDataUrl = process.env.RECEITAWS_DATA_URL || 'http://receitaws-data:3000';
